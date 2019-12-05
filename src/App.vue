76590107b60740245ed03358760acf760739e1ff
VIXI0 <mickycanela@gmail.com>
2019-12-05 02:31:16 -0400

    added new colors theme vuetify added page toolbar with custom logo and search bar based on template

--- src/App.vue
+++ src/App.vue
@@ -4,37 +4,48 @@
       app
       color="primary"
       dark
+      elevate-on-scroll
     >
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="@/assets/logos/honda-logo.png"
           transition="scale-transition"
           width="40"
         />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <span class="logo1">ODALIS</span>
+        <span class="logo2">MOTORS</span>        
       </div>
 
+        <v-autocomplete
+          v-model="select"
+          :loading="loading"
+          :items="items"
+          :search-input.sync="search"
+          cache-items
+          class="mx-4"
+          flat
+          hide-no-data
+          hide-details
+          label="Buscar Producto, Marca, Modelo, etc..."
+          solo-inverted
+          append-icon="mdi-magnify"
+        ></v-autocomplete>
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+            <template v-slot:extension>
+              <v-tabs
+                align-with-title
+                background-color="transparent"
+              >
+                <v-tab>Tab 1</v-tab>
+                <v-tab>Tab 2</v-tab>
+                <v-tab>Tab 3</v-tab>
+              </v-tabs>
+            </template>
     </v-app-bar>
 
     <v-content>
@@ -54,7 +65,52 @@ export default {
   },
 
   data: () => ({
-    //
+
+    loading: false,
+    items: [],
+    search: null,
+    select: null,
+    states: [
+          'Alabama',
+          'Alaska',
+          'American Samoa',
+          'Arizona',
+          'Arkansas'
+    ]
   }),
+
+  watch: {
+    search (val) {
+      val && val !== this.select && this.querySelections(val)
+      },
+    },
+  methods: {
+    querySelections (v) {
+      this.loading = true
+      // Simulated ajax query
+      setTimeout(() => {
+        this.items = this.states.filter(e => {
+          return (e || '').toLowerCase().indexOf((v || '').toLowerCase()) > -1
+        })
+        this.loading = false
+      }, 500)
+    },
+  },
 };
 </script>
+
+<style lang="sass" scoped>
+$font-logo:  "Arial Black", Gadget, sans-serif
+$primary-color-logo: #ffffff
+$secodary-color-logo: #cc0000
+
+span.logo1
+   font-family: $font-logo
+   color: $primary-color-logo
+   height: 50%
+
+span.logo2
+   font-family: $font-logo
+   color: $secodary-color-logo
+
+</style>
