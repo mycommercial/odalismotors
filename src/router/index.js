13e6b34f46afb8c07986b6a3a9b771a6db3e0a5b
VIXI0 <mickycanela@gmail.com>
2020-01-22 18:49:19 -0400

    activated propperties to productos

--- src/router/index.js
+++ src/router/index.js
@@ -13,17 +13,12 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
-    path: '/productos',
+    path: '/productos:Department',
     name: 'productos',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    props: true,
     component: () => import(/* webpackChunkName: "about" */ '../views/Productos.vue')
   }
 ]
