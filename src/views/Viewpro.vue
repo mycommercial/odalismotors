6e5cdcda1b16159ee15f9a336c4f3ce53e9beee8
VIXI0 <mickycanela@gmail.com>
2020-01-23 17:05:29 -0400

    new route to see products

---
+++ src/views/Viewpro.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+
+  </v-app>
+</template>
+
+<script>
+export default {
+name: 'viewpro',
+  components: {
+  },
+  props: {
+      //id product to see
+    _id: String,
+    //Selected in stock
+    selstock: String
+  },
+    data: () => ({
+
+    })
+}
+</script>
+
+<style>
+
+</style>
