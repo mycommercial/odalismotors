2d72190060e8b533750da8950d1184a0095fda0d
VIXI0 <mickycanela@gmail.com>
2020-08-12 01:29:46 -0400

    controlled duplicated route on search

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -50,7 +50,17 @@
 
     methods: {
       load(Suggest) {
-        this.$router.push({name: 'productos', params: { Department: "all" }, query: { q: Suggest }});
+        this.$router.push({name: 'productos', params: { Department: "all" }, query: { q: Suggest }})
+        .catch(err => {
+    // Ignore the vuex err regarding  navigating to the page they are already on.
+    if (
+      err.name !== 'NavigationDuplicated' &&
+      !err.message.includes('Avoided redundant navigation to current location')
+    ) {
+      // But print any other errors to the console
+      console.log(err);
+    }
+  });
         this.entries = [];
       }
     },
